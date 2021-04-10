d3bd77e31cc5e1ca5ef94ccc11635c035513e994
Bram Neijt <bram@neijt.nl>
2021-04-10 09:21:53 +0200

    Black

--- poetry_lock_package/__init__.py
+++ poetry_lock_package/__init__.py
@@ -1,8 +1,7 @@
-
 try:
     from importlib.metadata import version
 except:
-    #old version import
+    # old version import
     from importlib_metadata import version
 
 __version__ = version(__name__)
