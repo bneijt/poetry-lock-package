cf1117d96b68146cb8fc75afe1f320b8d123ff65
Bram Neijt <bram@neijt.nl>
2021-04-11 09:08:58 +0200

    Walk dependencies in lock based on project dependencies

--- poetry_lock_package/app.py
+++ poetry_lock_package/app.py
@@ -1,40 +1,91 @@
 import toml
 import os
 import click
+from typing import Dict, List
 from loguru import logger
+import copy
+import re
+import sys
 
 
-def dependency_spec(lock_information, dependency_name):
-    dependency_information = next(
-        filter(lambda x: x["name"] == dependency_name, lock_information["package"])
-    )
-
-    attributes = ["version", "python", "markers"]
-    spec = {}
-    for attr in attributes:
-        attr_value = dependency_information.get(attr)
-        if attr_value and attr_value != "*":
-            spec[attr] = attr_value
-    if list(spec.keys()) == ["version"]:
-        return spec["version"]
-    return spec
+def normalize_package_name(name: str) -> str:
+    # TODO probabaly somewhere in poetry-core or poetry
+    # see https://www.python.org/dev/peps/pep-0426/
+    return re.sub(r"[-_.]+", "-", name).lower()
 
 
-def read_lock_versions_for(direct_dependencies):
-    with open("poetry.lock", "r") as lock_file:
-        lock_toml = toml.load(lock_file)
+def collect_dependencies(lock_toml, package_names: List[str]):
+    def read_lock_information(name: str):
+        """select lock information for given dependency"""
+        for locked_package in lock_toml["package"]:
+            if locked_package["name"] == name or locked_package[
+                "name"
+            ] == normalize_package_name(name):
+                return copy.deepcopy(locked_package)
+        raise KeyError(f"Could not find '{name}' in lock file")
 
-        dependencies = {
-            dependency_name: dependency_spec(lock_toml, dependency_name)
-            for dependency_name in direct_dependencies
+    collected = {name: read_lock_information(name) for name in package_names}
+    del package_names
+
+    # Walk tree
+    for _ in range(1000):
+        dependencies_to_lock = {}
+        for _, lock_information in collected.items():
+            if "dependencies" in lock_information:
+                # Bug here: we are only collecting a single marker, overwriting multiple mentions
+                dependencies_to_lock.update(lock_information["dependencies"])
+                del lock_information["dependencies"]
+
+        if len(dependencies_to_lock) == 0:
+            break
+
+        lock_information = {
+            package_name: read_lock_information(package_name)
+            for package_name in dependencies_to_lock.keys()
         }
-        return dependencies
+
+        # merge dependencies (contains markers) and locks (contains versions)
+        for package_name, dependency_attributes in dependencies_to_lock.items():
+            if type(dependency_attributes) == str:
+                # If this is only a version, ignore it
+                dependency_attributes = {}
+            del_keys(dependency_attributes, ["version"])
+            lock_information[package_name].update(dependency_attributes)
+
+        collected.update(lock_information)
+    return collected
 
 
-def read_project():
-    with open("pyproject.toml", "r") as project_file:
-        project_toml = toml.load(project_file)
-        return project_toml
+def del_keys(dictionary: Dict, keys: List[str]) -> None:
+    """inplace deletion of given keys"""
+    for k in keys:
+        if k in dictionary:
+            del dictionary[k]
+
+
+def clean_dependencies(dependencies: Dict) -> Dict:
+    dependencies = copy.deepcopy(dependencies)
+    for _, metadata in dependencies.items():
+        del_keys(metadata, ["description", "category", "name", "extras", "source"])
+
+        if not metadata.get("optional"):
+            del_keys(metadata, ["optional"])
+        if "python-versions" in metadata:
+            metadata["python"] = metadata["python-versions"]
+            del metadata["python-versions"]
+            if metadata["python"] == "*":
+                del metadata["python"]
+
+    # Collapse version to single string
+    for name, metadata in dependencies.items():
+        if metadata.keys() == set(["version"]):
+            dependencies[name] = metadata["version"]
+    return dependencies
+
+
+def read_toml(filename: str):
+    with open(filename, "r") as project_file:
+        return toml.load(project_file)
 
 
 def lock_package_name(project_name: str) -> str:
@@ -46,14 +97,20 @@ def lock_package_name(project_name: str) -> str:
 @click.command(help="Generate a poetry lock package project from a poetry project")
 @click.option("--tests/--no-tests", default=True)
 def main(tests):
+    logger.remove()
+    logger.add(sys.stdout, colorize=True, format="<level>{level}</level> {message}")
     run(should_create_tests=tests)
-    logger.info("Done")
 
 
 def run(should_create_tests: bool) -> None:
-    project = read_project()
-    dependencies = read_lock_versions_for(
-        [k for k in project["tool"]["poetry"]["dependencies"].keys() if k != "python"]
+    project = read_toml("pyproject.toml")
+    lock = read_toml("poetry.lock")
+
+    project_root_dependencies = [
+        k for k in project["tool"]["poetry"]["dependencies"].keys() if k != "python"
+    ]
+    dependencies = clean_dependencies(
+        collect_dependencies(lock, project_root_dependencies)
     )
     dependencies["python"] = project["tool"]["poetry"]["dependencies"]["python"]
     project["tool"]["poetry"]["name"] = lock_package_name(
@@ -69,6 +126,7 @@ def run(should_create_tests: bool) -> None:
 
 def create_or_update(project, should_create_tests: bool):
     lock_project_path = project["tool"]["poetry"]["name"]
+    logger.info(f"Writing {lock_project_path}")
 
     # Create module folder
     module_path = os.path.join(
--- tests/test_poetry_lock_package.py
+++ tests/test_poetry_lock_package.py
@@ -1,15 +1,42 @@
-from poetry_lock_package.app import run, lock_package_name
+from poetry_lock_package.app import (
+    run,
+    lock_package_name,
+    clean_dependencies,
+    collect_dependencies,
+)
 import shutil
+import toml
 
 
 def test_main():
     try:
         run(True)
     finally:
-        shutil.rmtree("poetry-lock-package-lock")
+        shutil.rmtree("poetry-lock-package-lock", ignore_errors=True)
 
 
 def test_lock_package_name():
     assert lock_package_name("a") == "a-lock"
     assert lock_package_name("a-b") == "a-b-lock"
     assert lock_package_name("a_b") == "a_b_lock"
+
+
+def test_collect_dependencies():
+    with open("tests/resources/example1.lock", "r") as lock_file:
+        lock_toml = toml.load(lock_file)
+        assert clean_dependencies(
+            collect_dependencies(lock_toml, ["atomicwrites"])
+        ) == {
+            "atomicwrites": {
+                "python": ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+                "version": "1.4.0",
+            }
+        }
+
+        assert clean_dependencies(collect_dependencies(lock_toml, ["loguru"]))[
+            "win32-setctime"
+        ] == {
+            "markers": 'sys_platform == "win32"',
+            "python": ">=3.5",
+            "version": "1.0.3",
+        }
