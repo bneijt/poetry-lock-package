7409a9b76468e5fedf77c9b8ef14efc1e952ab66
Bram Neijt <bram@neijt.nl>
2021-04-12 12:14:05 +0200

    New feature: --wheel to build the lock project

--- poetry_lock_package/__init__.py
+++ poetry_lock_package/__init__.py
@@ -1,7 +1,7 @@
 try:
     from importlib.metadata import version
-except:
-    # old version import
+except ImportError:
+    # compatability import
     from importlib_metadata import version
 
 __version__ = version(__name__)
