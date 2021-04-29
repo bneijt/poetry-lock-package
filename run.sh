37f8b4cde964698605973b34c7dd0845f16a5a8d
Bram Neijt <bram@neijt.nl>
2021-04-29 18:37:00 +0200

    Add --build to --wheel --move --clean

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 poetry run poetry-lock-package --help
-poetry run poetry-lock-package --wheel --ignore 'imm..ables' --ignore 'contextvars' --no-parent
+poetry run poetry-lock-package --ignore 'imm..ables' --ignore 'contextvars' --no-parent
 cat poetry-lock-package-lock/pyproject.toml
+poetry run poetry-lock-package --build --ignore 'imm..ables' --ignore 'contextvars' --no-parent
