ebb3fbba9b89ea7f0d3dc6c09bf6a32bccbc3e42
Bram Neijt <bram@neijt.nl>
2021-05-18 08:44:54 +0200

    Remove most dependencies, broaden dependency ranges

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 poetry run poetry-lock-package --help
-poetry run poetry-lock-package --ignore 'imm..ables' --ignore 'contextvars' --no-parent
+poetry run poetry-lock-package --ignore 'imm..ables' --ignore 'contextvars' --no-root
 cat poetry-lock-package-lock/pyproject.toml
-poetry run poetry-lock-package --build --ignore 'imm..ables' --ignore 'contextvars' --no-parent
+poetry run poetry-lock-package --build --ignore 'imm..ables' --ignore 'contextvars' --no-root
