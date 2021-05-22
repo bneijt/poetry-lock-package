bb0f678252a277d557b09803ef241985695f7085
Bram Neijt <bram@neijt.nl>
2021-05-22 15:58:04 +0200

    Install black separately

--- try_plugin.sh
+++ try_plugin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #Build the docker container and run the in_docker_test.sh script to test
+set -e
+cd "$(dirname "$0")"
+rm -rf dist
 poetry build
 docker build -t poetry-lock-test .
 docker run -v `pwd`:/project:ro -it poetry-lock-test $@
