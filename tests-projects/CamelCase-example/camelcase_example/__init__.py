1c7806bc3dfe1addb3a07cfa02eea031d91ffd24
Bram Neijt <bram@neijt.nl>
2021-08-01 10:28:06 +0200

    Add tests projects to help reproduce issues

---
+++ tests-projects/CamelCase-example/camelcase_example/__init__.py
@@ -0,0 +1,2 @@
+def hello_from_camelcase():
+    print("Hello from CamcelCsae example")
---
+++ tests-projects/run_examples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+echo BUILD ALL
+for d in */; do
+(
+	set -e
+	cd "$d"
+	poetry lock
+	poetry install
+	poetry build
+	../../.venv/bin/poetry-lock-package --build
+	.venv/bin/pip install dist/*_lock*.whl
+)
+done
